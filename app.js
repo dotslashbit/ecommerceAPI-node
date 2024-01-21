e76b8d19fd3846bac01abd6dd79e717ef937e69e
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-22 01:31:34 +0530

    finished setting up db and basic express server

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+require("dotenv").config();
+require("express-async-errors");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 const port = process.env.PORT || 5001;
+
+// database
+const connectDB = require("./db/connect");
+
+//middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ECommerce API");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log(`Server listening on port ${port}`));
   } catch (e) {
     console.log(e);
